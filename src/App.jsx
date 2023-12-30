2267dca97ffc479d9da67bab21dae4a9bf13e0d2
Mnnan Mazhar <128976503+mnnandev@users.noreply.github.com>
2023-12-30 19:36:19 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Skill from "./components/Skills/Skill";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skill />
       </main>
     </>
   );
